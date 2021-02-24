dfe00c468a841e65bbe45d2bcfdc59de3e1e9dee
Ilze <gaile.ilze.89@gmail.com>
2021-02-24 16:50:34 +0200

    added scanner

--- src/Pub.java
+++ src/Pub.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Pub {
 
@@ -6,46 +6,58 @@ public class Pub {
         ONE_BEER, ONE_CIDER, A_PROPER_CIDER, GT, BACARDI_SPECIAL
     }
 
-    Drinks drink;
+    static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println("Total price:");
-        System.out.println(computeCost(Drinks.GT, true, 2));
+        System.out.println("Choose your drink (ONE_BEER, ONE_CIDER, A_PROPER_CIDER, GT, BACARDI_SPECIAL):");
+        Drinks drink = Drinks.valueOf(input.nextLine());
+
+        System.out.println("Are you a student? (true/false)");
+        boolean student = input.nextBoolean();
+
+        System.out.println("How many drinks do you want?");
+        int amount = input.nextInt();
+
+        System.out.println("And the total is:");
+        System.out.println(computeCost(drink, student, amount));
+        System.out.println();
+        System.out.println();
     }
 
-    public static int computeCost(Drinks drink, boolean student, int amount) {
+    public static double computeCost(Drinks drink, boolean student, int amount) {
 
+        double price = 0;
         if (amount > 2 && (drink == Drinks.GT || drink == Drinks.BACARDI_SPECIAL)) {
-            System.out.println("Too many drinks, max 2.");
-        }
+            System.out.println("Too many strong drinks for you friend, I can give you max 2.");
 
-        int price = 0;
-        switch (drink) {
-            case ONE_BEER:
-                price = 74;
-                break;
-            case ONE_CIDER:
-                price = 103;
-                break;
-            case A_PROPER_CIDER:
-                price = 110;
-                break;
-            case GT:
-                price = 115;
-                break;
-            case BACARDI_SPECIAL:
-                price = 127;
-                break;
-            default:
-                System.out.println("No such drink exists");
+        } else {
+
+            switch (drink) {
+                case ONE_BEER:
+                    price = 74;
+                    break;
+                case ONE_CIDER:
+                    price = 103;
+                    break;
+                case A_PROPER_CIDER:
+                    price = 110;
+                    break;
+                case GT:
+                    price = 115;
+                    break;
+                case BACARDI_SPECIAL:
+                    price = 127;
+                    break;
+                default:
+                    System.out.println("No such drink exists");
+            }
         }
 
         if (student && (drink == Drinks.ONE_CIDER || drink == Drinks.ONE_BEER || drink == Drinks.A_PROPER_CIDER)) {
             price = price - price / 10;
         }
-
         return price * amount;
-    }
 
+    }
 }
